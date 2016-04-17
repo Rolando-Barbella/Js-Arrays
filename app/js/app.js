46cb4724b15c4c56d49336ed71dadaf5e114a09e
Rolando-Barbella <rolandobarbella@gmail.com>
2016-04-17 15:26:28 +0100

    Added filter() examples to a separete file

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@ var greet = require('./array-in-depth/join');
 var greet = require('./array-in-depth/index-off');
 var greet = require('./array-in-depth/slice');
 var greet = require('./array-in-depth/sort');
+var greet = require('./array-in-depth/filter');
 
 
 /*greet('Shane');*/
--- app/js/array-in-depth/concat.js
+++ app/js/array-in-depth/concat.js
@@ -17,56 +17,9 @@ people
 	});
 
 
-/*
-	//Array filter()
-	//1
-var task = [1,2,3,4,5,6];
 
-var filtered = task.filter(x=>x>3);
 
-console.log(filtered);
-//2
-var people = [{name:'Rolando',pets:['cat','dog']},{name:'Pedro',pets:['horse']},{name:'Luis',pets:[]}];
-
-var filter2 = people.filter(x=>x.pets);
-console.log(filter2)
-//3
-const lessons = [
-    {
-        title: 'Javascript Arrays in Depth - join',
-        views: 960,
-        tags: ['array', 'join']
-    },
-    {
-        title: 'Javascript Arrays in Depth - concat',
-        views: 1050,
-        tags: ['array', 'concat']
-    },
-    {
-        title: 'Javascript Arrays in Depth - slice',
-        views: 2503,
-        tags: ['array', 'slice']
-    },
-    {
-        title: 'Javascript Functions in Depth - bind',
-        views: 2500,
-        tags: ['functions', 'bind']
-    }
-];
-
-var minViews = 1000;
-var searchTerm = 'array';
-
-var filter = lessons
-		.filter(x=>x.tags.indexOf(searchTerm)>-1)
-		.filter(x =>x.views>minViews)
-		.sort((a,b)=>b.views - a.views)
-		.map(x =>`<Li>${x.title}</li>`)
-		.join('\n');
-
-		console.log(`<ul>${filter}</ul>`);
-
-//Array some()
+/*//Array some()
 //1
 var numbers = [1,2,3,4,5,6];
 var hasThree = numbers.some(x=>x===3);
