bc2cd034b0242089b14ad31da4b8582419ff2308
Rolando-Barbella <rolandobarbella@gmail.com>
2016-06-06 22:26:20 +0100

    Reduce arguments example added

---
+++ app/js/reduce/reducer-arguments.js
@@ -0,0 +1,12 @@
+function	reducer(accumulator,value,index,array){
+	var intermediaryValue = accumulator + value;
+	if(index === array.length -1){
+		return intermediaryValue / array.length;
+	}
+	return accumulator + value;
+}
+
+var data = [1,2,3,3,4,5,3];
+var mean = data.reduce(reducer,0);
+
+console.log(mean);
